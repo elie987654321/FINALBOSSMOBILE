fc47f899aa7403331364b4414cbddfdcef92b0cc
Élie Poirier.-Clément <elie987654321@gmail.com>
2025-12-11 10:03:42 -0500

    Creer interface voire commandes

---
+++ Mobile/app/src/main/java/com/example/myapplication/ActivitiesAndFragments/VoirCommandeFragment.java
@@ -0,0 +1,25 @@
+package com.example.myapplication.ActivitiesAndFragments;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+public class VoirCommandeFragment extends Fragment
+{
+    ListView listViewPizzaFragment;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        
+    }
+}
